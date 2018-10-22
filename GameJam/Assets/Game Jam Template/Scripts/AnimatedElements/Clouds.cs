aa0804efdca36552b29a60b95d6e7e814fa0f377
RosieEve <sciarrillo.nella@gmail.com>
2018-10-22 21:51:09 +0100

    MainScreen Added
    UIElements Added

---
+++ GameJam/Assets/Game Jam Template/Scripts/AnimatedElements/Clouds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 pos1;
+    [SerializeField] private Vector3 pos2;
+    public Vector3 posDiff = new Vector3(0f, 0f, 20f);
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        pos1 = transform.position;
+        pos2 = transform.position + posDiff;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+}
---
+++ GameJam/Assets/Game Jam Template/Scripts/AnimatedElements/CurrentElementDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentElementDisplay : MonoBehaviour {
+
+    public Sprite[] ElementSprites;  //0 --> Air, 1 --> water, 2--> Earth, 3--> Fire
+    private Image sprite;
+
+	// Use this for initialization
+	void Start () {
+
+        sprite = gameObject.GetComponent<Image>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CurrentElement();
+
+	}
+
+
+    void CurrentElement()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            sprite.sprite = ElementSprites[0];
+            sprite.color = Color.white;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            sprite.sprite = ElementSprites[1];
+            sprite.color = Color.white;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            sprite.sprite = ElementSprites[2];
+            sprite.color = Color.white;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            sprite.sprite = ElementSprites[3];
+            sprite.color = Color.white;
+        }
+    }
+}
